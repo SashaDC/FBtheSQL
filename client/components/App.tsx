e5d306012e4db6bd9c19bc2e4b251dcba7915368
Sasha Chambers <sashachambers10.12@gmail.com>
2025-10-10 16:06:12 +1300

    Made the App.tsx layout functional for routes.

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,19 @@
-import UserPage from './UserPage'
+import { Outlet } from 'react-router'
+
+// I've made it so that it renders things on the Home.tsx component via the outlet/routes.
+// I've also added the "Semantic Elements" for later use. (eg. header, main, footer.)
 
 function App() {
   return (
     <>
       <div className="app">
-        <h1>FB the Sequel</h1>
-        <UserPage />
+        <header>
+          <h1>FB the Sequel</h1>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer></footer>
       </div>
     </>
   )
