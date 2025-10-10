d3f8f891a5e3e27c3bf100ad9b4ac2a99a50a85e
cypress <comrie1.cc@gmail.com>
2025-10-10 17:37:07 +1300

    get ready to set up commands for posts

--- models/posts.ts
+++ models/posts.ts
@@ -5,3 +5,10 @@ export interface posts {
   date: string
   user_id: number
 }
+// commands file
+// display posts
+// create posts
+//update posts
+// delete posts
+//search by user id
+// search by date
