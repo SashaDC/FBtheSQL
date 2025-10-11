7466d1f5589fe60c39d07a7a2b49cc82404fd2aa
Luis Ramirez <mrluisramirez24@gmail.com>
2025-10-11 17:47:35 +1300

    Update post and users, query hook

--- client/apis/posts.ts
+++ client/apis/posts.ts
@@ -7,3 +7,24 @@ export async function getPosts() {
   const response = await request.get(`${rootURL}/posts`)
   return response.body as posts[]
 }
+
+// Added
+
+// Create a new post
+// we can remove Omit if the database will
+// have an auto numbering in our database
+
+export async function addNewPost(data: Omit<posts, 'id'>): Promise<number> {
+  const response = await request.post(`${rootURL}/posts/new`).send(data)
+  return response.body
+}
+
+//Update an existing post
+export async function updatePost(data: posts): Promise<posts> {
+  const response = await request.patch(`${rootURL}/posts/edit`).send(data)
+  return response.body
+}
+
+export async function deletePostById(id: number): Promise<void> {
+  await request.delete(`${rootURL}/posts/delete`).send({ id })
+}
---
+++ client/hooks/usePosts.ts
@@ -0,0 +1,51 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+
+import {
+  getPosts,
+  addNewPost,
+  updatePost,
+  deletePostById,
+} from '../apis/posts.ts'
+import { posts } from '../../models/posts.ts'
+
+//Get all posts
+export function useGetPosts() {
+  const query = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+  })
+  return query
+}
+
+//  Add a new post
+export function useAddNewPost() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: addNewPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+    },
+  })
+}
+
+// Update a post
+export function useUpdatePost() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: updatePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+    },
+  })
+}
+
+// Delete a post
+export function useDeletePost() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: deletePostById,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+    },
+  })
+}
